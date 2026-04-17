f7653d23475045a3d246a3592c455746d5842fd9
mnahiun <duartemateusnahin701@gmail.com>
2026-04-16 20:39:18 -0500

    feat: Creamos clase CategoriaControl, pero aun  nos falta terminar los metodos de actualizar, desactivar, activar y total

---
+++ src/negocio/CategoriaControl.java
@@ -0,0 +1,80 @@
+
+package negocio;
+
+import datos.CategoriaDAO;
+import entidades.Categoria;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.DefaultTableModel;
+
+public class CategoriaControl {
+    private final CategoriaDAO DATOS;
+    private Categoria obj;
+    private DefaultTableModel modeloTabla;
+
+    public CategoriaControl(){
+     this.DATOS= new CategoriaDAO();
+     this.obj = new Categoria();
+
+    }
+
+    public DefaultTableModel listar(String texto){
+     List<Categoria> lista = new ArrayList<>();
+     lista.addAll(DATOS.listar(texto));
+
+     String[] titulos = {"Id", "Nombre", "Descripcion", "Estado"};
+     this.modeloTabla = new DefaultTableModel(null, titulos);
+
+     String estado;
+     String[] registro =  new String[4];
+
+     for(Categoria item:lista){
+        if(item.isActivo()){
+        estado = 'Activo';
+
+        }else{
+            estado = 'inactivo';
+        }
+
+        
+        registro [0] = Integer.toString(item.getId());
+        registro [1] = item.getNombre();
+        registro [2] = item.getDescripcion();
+        registro [3] = estado;
+        this.modeloTabla.addRow(registro);
+
+     }
+       return this.modeloTabla;
+    }
+
+    public String insertar(String nombre, String descripcion){
+    if(DATOS.existe(nombre)){
+        return "El registro ya existe";
+    }else{
+        obj.setNombre(nombre);
+        obj.setDescripcion(descripcion);
+        if(DATOS.insertar(obj)){
+            
+        }
+    }
+    }
+
+    public String actualizar(int id, String nombre, String nombreAnt, String descripcion){
+
+
+    }
+
+    public String desactivar(int id){
+
+    }
+
+    public String activar(int id){
+
+
+    }
+
+    public int total(){
+
+    }
+    
+}
