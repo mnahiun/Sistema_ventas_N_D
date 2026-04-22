28c9ca9317baa9f8d462b9633223c9bbf0cb179b
mnahiun <duartemateusnahin701@gmail.com>
2026-04-21 19:21:43 -0500

    feature(negocio): Terminamos clase  CategoriaControl.java

--- src/negocio/CategoriaControl.java
+++ src/negocio/CategoriaControl.java
@@ -64,15 +64,48 @@ public class CategoriaControl {
     }
 
     public String actualizar(int id, String nombre, String nombreAnt, String descripcion){
-       
+        if (nombre.equals(nombreAnt)) {
+            obj.setId(id);
+            obj.setNombre(nombre);
+            obj.setDescripcion(descripcion);
+            if (DATOS.actualizar(obj)) {
+                return "OK";
+            }else{
+                return "Error en la actualización";
+                
+                }
+            
+        }else {
+            if (DATOS.existe(nombre)) {
+                return "El registro ya existe";
+            }else{
+               obj.setId(id);
+               obj.setNombre(nombre);
+               obj.setDescripcion(descripcion);
+                if (DATOS.actualizar(obj)) {
+                    return "OK";
+                    
+                }else{
+                 return "Error en la actualización";              
+                }
+            }
+        }
     }
 
     public String desactivar(int id){
-  
+        if (DATOS.desactivar(id)) {
+            return "OK";
+        }else{
+            return "No se puede desactivar el registro";
+        }
     }
 
     public String activar(int id){
-    
+         if (DATOS.activar(id)) {
+            return "OK";
+        }else{
+            return "No se puede activar el registro";
+        }
 
     }
 
