190c5cd576bd0156137d2634f9578f4386897391
mnahiun <duartemateusnahin701@gmail.com>
2026-05-12 19:31:07 -0500

    feat: se crearon mas entidades

--- src/database/Conexion.java
+++ src/database/Conexion.java
@@ -1,8 +1,5 @@
-<<<<<<< Updated upstream
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
+
 package database;
 
 import java.sql.Connection;
@@ -52,55 +49,5 @@ public class Conexion {
         return instancia;
     }
 }
-=======
- 
-package database;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import javax.swing.JOptionPane;
 
-public class Conexion {
- 
-    private final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private final String URL = "jdbc:mysql://localhost:3308/";
-    private final String DB = "dbsistema";
-    private final String USER = "root";
-    private final String PASSWORD = "";
-    
-    
-    public Connection cadena;
-    public static Conexion instancia;
-    
-    private Conexion (){
-         this.cadena = null;    
-    }
-    
-    public Connection conectar(){
-        try {
-            Class.forName(DRIVER);
-            this.cadena = DriverManager.getConnection(URL+DB,USER,PASSWORD);
-        } catch (ClassNotFoundException | SQLException e) {
-            JOptionPane.showMessageDialog(null, e.getMessage());
-        }
-        
-        return this.cadena;
-    }
-    
-    
-    public void desconectar(){
-        try {
-            this.cadena.close();
-        } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null, e.getMessage());
-        }
-    }
-    public synchronized static Conexion getInstancia(){
-        if (instancia==null) {
-            instancia = new Conexion();
-        }
-        return instancia;
-    }
-}
->>>>>>> Stashed changes
